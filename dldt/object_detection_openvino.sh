53c72943ccb4fdf40b1dad5c8873cbfec99ec890
VINOD KUMAR DEVARAMPATI <25518604+vdevaram@users.noreply.github.com>
2019-10-31 12:24:57 +0800

    Update object_detection_openvino.sh

--- dldt/object_detection_openvino.sh
+++ dldt/object_detection_openvino.sh
@@ -32,3 +32,24 @@ rm -rf $VINO_WORKSPACE/demos/*
 cd $VINO_WORKSPACE/demos
 cmake $VINO_HOME/intel/openvino/deployment_tools/inference_engine/demos/
 #################### installation and setup complete #######################
+#### initialize environment #####
+export VINO_HOME=$HOME  or export VINO_HOME=/opt
+export VINO_WORKSPACE=$HOME/VINO
+source $VINO_HOME/intel/openvino/bin/setupvars.sh
+source ~/.vino_env/bin/activate
+#FRCNN for Object detection
+mkdir -p $VINO_WORKSPACE/models
+cd $VINO_WORKSPACE/models
+tar -zxvf faster_rcnn_resnet50_coco_2018_01_28.tar.gz
+cd faster_rcnn_resnet50_coco_2018_01_28
+#FP32 conversion 
+python3 $VINO_HOME/intel/openvino/deployment_tools/model_optimizer/mo.py --framework tf --input_model frozen_inference_graph.pb     --output_dir  ./fp32/ --output=detection_boxes,detection_scores,num_detections --tensorflow_use_custom_operations_config $VINO_HOME/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/faster_rcnn_support.json --tensorflow_object_detection_api_pipeline_config pipeline.config
+#FP16 Conversion
+python3 $VINO_HOME/intel/openvino/deployment_tools/model_optimizer/mo.py --framework tf --input_model frozen_inference_graph.pb     --output_dir  ./fp16/ --output=detection_boxes,detection_scores,num_detections --tensorflow_use_custom_operations_config $VINO_HOME/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/faster_rcnn_support.json --tensorflow_object_detection_api_pipeline_config pipeline.config --data_type FP16
+#INT8 conversion
+## TBD
+# Benchmark 
+$VINO_WORKSPACE/samples/intel64/Release/benchmark_app -m $VINO_WORKSPACE/models/faster_rcnn_resnet50_coco_2018_01_28/fp32/frozen_inference_graph.xml
+# Python Demo sample usage 
+ $VINO_HOME/intel/openvino/deployment_tools/inference_engine/demos/python_demos/object_detection_demo_ssd_async/object_detection_demo_ssd_async.py -i "cam" -m $VINO_WORKSPACE/models/faster_rcnn_resnet50_coco_2018_01_28/fp32/frozen_inference_graph.xml -d CPU  -l $VINO_WORKSPACE/samples/intel64/Release/lib/libcpu_extension.so
+
