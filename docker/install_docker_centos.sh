e5efa08b4913b21e2d8e067fbb92d8e65a0fb1a9
VINOD KUMAR DEVARAMPATI <25518604+vdevaram@users.noreply.github.com>
2020-05-25 14:28:42 +0800

    Update install_docker_centos.sh

--- docker/install_docker_centos.sh
+++ docker/install_docker_centos.sh
@@ -17,7 +17,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 #sudo yum-config-manager --enable docker-ce-edge
 #sudo yum-config-manager --enable docker-ce-test
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 # proxy setup
